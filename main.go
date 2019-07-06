ed8513a7e80ef5b8a3427f64ddab741256c78e50
devops787 <devops787@gmail.com>
2019-07-06 10:52:05 +0300

    HTTP server & router

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+	"log"
+)
 
 func main() {
 	fmt.Println("RESTful API")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("RESTful API"))
+	})
+
+	server := &http.Server{
+		Handler: router,
+		Addr: "127.0.0.1:3000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
